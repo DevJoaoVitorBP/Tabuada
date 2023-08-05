37da1ddcd4e1a0dd5913445ed410dff31c5f2cd7
João Vitor <76665184+Joao-Pereira013@users.noreply.github.com>
2023-08-05 15:19:21 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,8 @@
+// Variavel com o número a ser feito a tabuada 
+const numero = 2;
+
+// Montando a estrutura for para executar a tabuada 
+for (let i = 0; i <= 10; i++) {
+    console.log(i * numero)
+
+}
